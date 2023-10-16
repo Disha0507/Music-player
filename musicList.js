98ec5c2538d942f66b15cd59ec3926c9a265cdab
disha0507 <dishasingh2022003@gmail.com>
2023-10-16 16:15:45 +0530

    first commit

---
+++ musicList.js
@@ -0,0 +1,45 @@
+let allmusic=[
+    {
+    Name:"At My Worst",
+    artist:"Pink Sweets",
+    img:"music1",
+    src:"music1"
+    },
+    {
+        Name:"At My Worst",
+        artist:"Pink Sweets",
+        img:"music2",
+        src:"music2"
+        },
+        {
+            Name:"Counting Stars",
+            artist:"Pink Sweets",
+            img:"music3",
+            src:"music3"
+            },
+            {
+                Name:"Dil Jhoom",
+                artist:"Pink Sweets",
+                img:"music4",
+                src:"music4"
+                },
+                {
+                    Name:"Memories",
+                    artist:"Pink Sweets",
+                    img:"music5",
+                    src:"music5"
+                    },
+                    {
+                        Name:"Night Changes",
+                        artist:"Pink Sweets",
+                        img:"music6",
+                        src:"music6"
+                        },
+                        {
+                            Name:"Talking to the moon",
+                            artist:"Pink Sweets",
+                            img:"music7",
+                            src:"music7"
+                            },
+
+];
